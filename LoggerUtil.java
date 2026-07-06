86b703e0c7dffa92290fa25b5b4540dd208b8a86
Sunidhi Ambadkar <sunidhiambadkar@gmail.com>
2026-07-06 22:20:45 +0530

    Update LoggerUtil.java

--- LoggerUtil.java
+++ LoggerUtil.java
@@ -1,1 +1,46 @@
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public final class LoggerUtil {
+
+    private static final Logger LOGGER =
+            Logger.getLogger("ChatApplication");
+
+    static {
+
+        LOGGER.setUseParentHandlers(false);
+
+        ConsoleHandler handler =
+                new ConsoleHandler();
+
+        handler.setLevel(Level.ALL);
+
+        LOGGER.addHandler(handler);
+
+        LOGGER.setLevel(Level.ALL);
+    }
+
+    private LoggerUtil() {
+
+    }
+
+    public static void info(String message) {
+
+        LOGGER.info(message);
+    }
+
+    public static void warning(String message) {
+
+        LOGGER.warning(message);
+    }
+
+    public static void error(String message,
+                             Exception exception) {
+
+        LOGGER.log(
+                Level.SEVERE,
+                message,
+                exception);
+    }
+}
