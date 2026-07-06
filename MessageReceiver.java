1a754437f96c63ca18881965692b202e24cc627b
Sunidhi Ambadkar <sunidhiambadkar@gmail.com>
2026-07-06 22:15:16 +0530

    Update MessageReceiver.java

--- MessageReceiver.java
+++ MessageReceiver.java
@@ -1,1 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 
+/**
+ * Receives messages from the server.
+ */
+public class MessageReceiver
+        implements Runnable {
+
+    private final BufferedReader input;
+
+    private final ChatClient client;
+
+    public MessageReceiver(
+            BufferedReader input,
+            ChatClient client) {
+
+        this.input = input;
+        this.client = client;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+
+            String message;
+
+            while ((message =
+                    input.readLine()) != null) {
+
+                client.appendMessage(
+                        message);
+            }
+
+        } catch (IOException e) {
+
+            client.appendMessage(
+                    "Connection Lost.");
+
+        } finally {
+
+            client.serverDisconnected();
+        }
+    }
+}
